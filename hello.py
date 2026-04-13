41adac7deca4b565e38b83afb32f5a3715662a41
uriel7899 <urieljonathand9@gmail.com>
2026-04-13 14:08:53 -0300

    Tipos de datos

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@
 Hola mundo
 """
 print("Hola Mundo")
+
+#Para consultar el tipo de dato
+print(type(10)) #Tipo 'int'
+
+print(type("Hola")) #Tipo 'str'
+
+print(type(True)) #Tipo 'bool'
+
+print(type(1.78)) #Tipo 'float'
